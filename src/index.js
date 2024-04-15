cd1561c41ece821c194e063d73160c1aa437d87c
OksanaMosk <oks-m3@ukr.net>
2024-04-15 19:55:20 +0300

    instal

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="rentalcars">
+    <BrowserRouter basename="PsychologistsServices">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
